1dcfe44a6d4e8c9663c0213f1ebae935f1de65d2
raykonopka <konopka.ray@gmail.com>
2016-12-01 12:55:03 -0500

    added tests

---
+++ PizzaStoreUI/PizzaStoreUI.Tests/MVCTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PizzaStoreUI.Tests
+{
+    public class MVCTests
+    {
+        //Test After API Is Published
+
+        [Fact]
+        public void Test_LoadOrderPage()
+        {
+            HttpClient httpClient = new HttpClient();
+
+            var actual = httpClient.GetAsync("http://34.193.163.157/pizza-store-mvc/PizzaStore/Order").Result;
+
+            if (actual.IsSuccessStatusCode)
+            {
+                Debug.WriteLine("Order Page Sucessfully Loaded.");
+            }
+            else
+            {
+                Debug.WriteLine("Order Page Failed To Load.");
+            }
+
+            Assert.True(actual.IsSuccessStatusCode);
+        }
+
+    }
+}
